7ae4cb24abb27dae28175c4758d5d46f39e2c9c2
plumppiggy <elysiadarbourne@yahoo.ca>
2025-12-04 13:35:26 -0500

    feat: day 4 part 2

--- day4/day4.py
+++ day4/day4.py
@@ -1,7 +1,25 @@
 def read_input():
-  with open("input.txt") as f:
+  with open("test.txt") as f:
     return f.read().strip().split("\n")
   
+def count_rolls(graph, n, m):
+  remove = []
+  for i in range(n):
+    for j in range(m):
+      if graph[i][j] == 1:
+        cnt = 0
+        neighbours = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+
+        for dx, dy in neighbours:
+          x, y = i + dx, j + dy
+
+          if 0 <= x < n and 0 <= y < m and graph[x][y] == 1:
+            cnt += 1
+
+        if cnt < 4:
+          remove.append((i, j))
+
+  return remove
 
 def main():
   rows = read_input()
@@ -34,6 +52,18 @@ def main():
           part_one += 1
 
   print("Part one:", part_one)
+
+  remove = count_rolls(graph, n, m)
+  while remove:
+    for i, j in remove:
+      part_two += 1
+      graph[i][j] = 0
+
+    remove = count_rolls(graph, n, m)
+
+  print("Part two:", part_two)
+
+  
 
 
   
